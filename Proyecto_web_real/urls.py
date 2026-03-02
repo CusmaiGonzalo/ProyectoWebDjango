607b51762a02746674ac39ce00072acd10610a2c
GonzaloCusmai <gonzaloleonelcusmai@gmail.com>
2026-03-02 10:58:32 -0300

    algunas modificaciones

--- Proyecto_web_real/urls.py
+++ Proyecto_web_real/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('servicios/', include('Servicios.urls')),
     path('', include('Proyecto_web_app.urls')),
 ]
--- Servicios/views.py
+++ Servicios/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from Servicios.models import Servicio
 
 # Create your views here.
+
+def Servicios(request):
+
+    servicios = Servicio.objects.all()
+    return render(request, 'Servicios/servicios.html', {'servicios': servicios})
